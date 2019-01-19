3534e578f1a80e133703f5df1593f604ac85c5bc
sudhir pradhan <rcmkkskp@gmail.com>
2019-01-20 00:22:57 +0530

    first commit from AS

--- app/src/main/java/com/sudhirpradhan/sensorslist/MainActivity.java
+++ app/src/main/java/com/sudhirpradhan/sensorslist/MainActivity.java
@@ -14,9 +14,7 @@ import java.util.List;
 
 public class MainActivity extends Activity {
     private SensorManager mSensorManager;
-
     TextView tvTextView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
